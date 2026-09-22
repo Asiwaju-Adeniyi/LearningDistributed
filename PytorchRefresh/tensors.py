592baa7dc543f1f2f9e0ddbc0e27d67e967c023d
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-09-22 16:11:56 +0100

    learning

--- PytorchRefresh/tensors.py
+++ PytorchRefresh/tensors.py
@@ -3,8 +3,6 @@ import numpy as np
 
 x = torch.rand(2,2)
 y = torch.rand(2,2)
-
-
 z = torch.sub(x, y)
 print(z)
 y.mul_(x)
@@ -13,5 +11,14 @@ y.mul_(x)
 
 a = np.ones(5)
 print(a)
-b = a.numpy()
+b = torch.from_numpy(a)
 print(b)
+
+if torch.mps.is_available(): 
+    device = torch.device("mps")
+    x = torch.ones(5, device=device)
+    y = torch.ones(5, device=device)
+    q = torch.zeros(5)
+    q = q.to(device)
+    z = x + y
+    print(z)
