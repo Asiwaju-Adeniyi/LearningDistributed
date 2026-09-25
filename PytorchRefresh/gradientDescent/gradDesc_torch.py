ec74f940743e5f15779db15134e892ed7442cd05
Adeniyi Makinde <makindeadeniyi1@gmail.com>
2026-09-25 01:20:40 +0100

    torch gradient descent

---
+++ PytorchRefresh/gradientDescent/gradDesc_torch.py
@@ -0,0 +1,46 @@
+import torch
+
+# f = w * x
+# f = 2 * x
+X = torch.tensor([1,2,3,4], dtype=torch.float32)
+Y = torch.tensor([2,4,6,8], dtype=torch.float32)
+
+w = 0.0
+
+#model prediction 
+def forward(x) : 
+    return w * x
+
+# loss = MSE
+def loss(y, y_predicted): 
+    return((y_predicted-y)**2).mean()
+
+# gradient 
+#MSE = 1/N * (w * x - y)**2
+#dJ/dw = 1/N 2x (w * x - y)\
+def gradient (x,y, y_predicted): 
+    return np.dot(2*x, y_predicted - y).mean()
+
+print(f'prediction before training: f(5) = {forward(5): .3f}')
+
+#Training
+learning_rate = 0.01
+n_iters = 10
+
+for epoch in range(n_iters): 
+    #prediction = forward pass 
+    y_pred = forward(x)
+
+    #loss 
+    l = loss(Y, y_predicted) 
+
+    #gradients
+    dW = gradient(X, Y, y_predicted)
+
+    #update weights
+    w -= learning_rate * dw 
+
+    if epoch % 1 == 0: 
+        print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {l:.8}')
+
+print(f'prediction after training: f(5) = {forward(5): .3f}')
